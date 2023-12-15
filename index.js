cfa8d96363c9bba66d84f5ba7fbebf54d38ce3aa
Austin Rivera <austin.rivera.24@aol.com>
2023-12-15 18:52:32 -0500

    For/While Loops

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// For Loop (Thank You Cards)
+const cards = ["Guadalupe","Ollie", "Aki"];
+const event = "birthday"
+const messageArray = writeCards(cards, event);
+
+function writeCards(names, eventName) {
+
+    let thankYouMessages = []
+
+    for (let i = 0; i < names.length; i++) {
+
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+// While Loop (Countdown)
+
+function countDown(startingNumber) {
+    let currentNumber = 10;
+
+    while (currentNumber >= 0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }    
+}
